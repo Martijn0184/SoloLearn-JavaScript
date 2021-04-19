d266c7cbe6a7b8dfad506010e4f383436d2e2ea9
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-04-19 17:43:17 +0200

    Add files via upload

---
+++ 030_The_if_Statements_III/index.js
@@ -0,0 +1,5 @@
+var myNum1 = 10;
+var myNum2 = 7;
+if (myNum1 < myNum2) {
+    document.write("OK");
+}
---
+++ 032_The_else_Statement_II/index.js
@@ -0,0 +1,7 @@
+var age = 25;
+if (age >= 18) {
+    document.write("Allowed");
+}
+else {
+    document.write("Not Allowed");
+}
