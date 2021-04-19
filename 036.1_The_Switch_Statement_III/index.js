da28271f0c94850db2c0a5970014114955ae74f4
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-04-19 20:11:58 +0200

    Add files via upload

---
+++ 036.1_The_Switch_Statement_III/index.js
@@ -0,0 +1,16 @@
+var day_of_week = 6;
+switch (day_of_week){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        document.write("Working Days");
+        break;
+    case 6:
+        document.write("Today is Saturday");
+        break;
+    case 7:
+        document.write("Today is Sunday");
+        break;
+}
