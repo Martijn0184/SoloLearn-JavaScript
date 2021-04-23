0b9b4e5ef4cc541482898392ee41b1e5bd621e0d
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-04-23 14:02:08 +0200

    Add files via upload

---
+++ 143_ES6_Classes_II/index.js
@@ -0,0 +1,12 @@
+let Square = class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+const square = new Square(5, 5);
+const poster = new Square(2, 3);
+
+console.log(square.height);
+console.log(poster.width);
